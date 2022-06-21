6a2b7ba10e9f3010584cd78d16ac39e289e22e0f
Israel <igarashiisrael@icloud.com>
2022-06-20 22:57:24 -0300

    Projeto Frontend atualizado

---
+++ src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { SobreOdsComponent } from './sobre-ods/sobre-ods.component';
+import { HomeComponent } from './home/home.component';
+import { AlertasComponent } from './alertas/alertas.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { OrderModule } from 'ngx-order-pipe';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    InicioComponent,
+    TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent,
+    SobreNosComponent,
+    SobreOdsComponent,
+    HomeComponent,
+    AlertasComponent,
+    UserEditComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ModalModule.forRoot(),
+    OrderModule
+  ],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>('https://projetoseiseduca.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  entrar(userLgin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://projetoseiseduca.herokuapp.com/usuarios/logar', userLgin)
+  }
+
+  editar(user: User): Observable<User> {
+    console.log(environment.token)
+    return this.http.put<User>('https://projetoseiseduca.herokuapp.com/usuarios/atualizar', user, this.token)
+  }
+
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(`https://projetoseiseduca.herokuapp.com/usuarios/${id}`, { headers: new HttpHeaders().set('Authorization', environment.token) })
+  }
+
+  logado() {
+    let ok: boolean = false;
+    if (environment.token != '') {
+      ok = true
+    }
+    return ok
+  }
+
+  deslogado() {
+    let ok: boolean = false;
+    if (environment.token == '') {
+      ok = true
+    }
+    return ok
+  }
+
+
+
+}
